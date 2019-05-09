dc81b3c7f41a83cbadfdb677bb07a0dfcc0bec15
quixote15 <santos2tiago@gmail.com>
2019-05-09 14:54:04 -0300

    Remove application controller

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -6,37 +6,7 @@ export default Controller.extend({
 
     current: '',
   
-    completed: computed( 'model', 'current', function() {
-      var tasks = this.get('model');
-      var current = this.get('current');
-  
-      let finished = current === 'complete';
-      let unfinished = current === 'incomplete';
-  
-      if (finished) {
-        return tasks.filter((task) => {
-          return task.get('isDone') === true;
-        });
-      }
-  
-      if (unfinished) {
-        return tasks.filter((task) => {
-          return task.get('isDone') === false;
-        });
-      }
-      return tasks;
-    }),
-  
     actions: {
-      showAll() {
-        this.set('current', '');
-      },
-      showCompleted() {
-        this.set('current', 'complete');
-      },
-      showIncomplete() {
-        this.set('current', 'incomplete');
-      },
       editTask(task) {
         this.transitionToRoute('task.edit', task.get('id'));
       }
